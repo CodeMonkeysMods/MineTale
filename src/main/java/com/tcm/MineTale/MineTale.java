3b5085991b0c781ad5cc13ca1feb3ffd3687c2bc
AnyaPizza <axolotlanya18@gmail.com>
2026-01-29 23:58:17 -0500

    Added Creative Tab

--- src/main/java/com/tcm/MineTale/MineTale.java
+++ src/main/java/com/tcm/MineTale/MineTale.java
@@ -2,6 +2,8 @@ package com.tcm.MineTale;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,9 @@ import com.tcm.MineTale.registry.ModEntities;
 import com.tcm.MineTale.registry.ModEntityDataSerializers;
 import com.tcm.MineTale.registry.ModItems;
 import com.tcm.MineTale.registry.ModMenuTypes;
+
+import static com.tcm.MineTale.item.ModCreativeTab.MINETALE_CREATIVE_TAB;
+import static com.tcm.MineTale.item.ModCreativeTab.MINETALE_CREATIVE_TAB_KEY;
 
 public class MineTale implements ModInitializer {
 	public static final String MOD_ID = "minetale";
@@ -33,6 +38,8 @@ public class MineTale implements ModInitializer {
 		ModMenuTypes.initialize();
 		ModEntities.initialize();
 		ModItems.initialize();
+
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, MINETALE_CREATIVE_TAB_KEY, MINETALE_CREATIVE_TAB);
 
 		ModEntityDataSerializers.initialize();
 
