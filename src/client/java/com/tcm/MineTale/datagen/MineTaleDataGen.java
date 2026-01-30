5da607c40a01ff0f800462d9126dae56b1f2979c
AnyaPizza <axolotlanya18@gmail.com>
2026-01-30 00:12:43 -0500

    Added Datagen and created datagen lang file. Added translations to blocks.

---
+++ src/client/java/com/tcm/MineTale/datagen/MineTaleDataGen.java
@@ -0,0 +1,14 @@
+package com.tcm.MineTale.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MineTaleDataGen implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        pack.addProvider(MineTaleLangProvider::new);
+    }
+}
--- src/main/java/com/tcm/MineTale/item/ModCreativeTab.java
+++ src/main/java/com/tcm/MineTale/item/ModCreativeTab.java
@@ -14,7 +14,7 @@ public class ModCreativeTab {
     public static final ResourceKey<CreativeModeTab> MINETALE_CREATIVE_TAB_KEY = ResourceKey.create(BuiltInRegistries.CREATIVE_MODE_TAB.key(), Identifier.fromNamespaceAndPath(MineTale.MOD_ID, "creative_tab"));
     public static final CreativeModeTab MINETALE_CREATIVE_TAB = FabricItemGroup.builder()
             .icon(() -> new ItemStack(ModBlocks.FURNACE_WORKBENCH_BLOCK))
-            .title(Component.literal("MineTale Stuffs"))
+            .title(Component.translatable("minetale.creative_tab.title"))
             .displayItems((params, output) -> {
                 output.accept(ModBlocks.FURNACE_WORKBENCH_BLOCK);
                 output.accept(ModBlocks.CAMPFIRE_WORKBENCH_BLOCK);
